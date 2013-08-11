432cc646c71380a52d305aa737a146e4aa2f5a25
Joseph Montanez <jmontanez@gorilla3d.com>
2013-08-11 06:35:41 -0700

    Update build.bat

--- database/build.bat
+++ database/build.bat
@@ -3,7 +3,7 @@ echo Building DBML
 "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\bin\SqlMetal.exe" /server:.\SQLEXPRESS /database:FSharpDB /context:DBContext  /dbml:database.dbml
 
 echo Compiling Application
-"C:\Program Files (x86)\Microsoft SDKs\F#\3.0\Framework\v4.0\Fsc.exe" -r FSharp.Data.TypeProviders.dll -r System.Data.Linq.dll db.fs
+"C:\Program Files (x86)\Microsoft SDKs\F#\3.0\Framework\v4.0\Fsc.exe" -r FSharp.Data.TypeProviders.dll -r System.Data.Linq.dll main.fs
 
 echo Running Application
-db.exe
+main.exe
