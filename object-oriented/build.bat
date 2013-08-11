989d73b11d798be5300ef65716a3dc9908cddee2
Joseph Montanez <jmontanez@gorilla3d.com>
2013-08-11 09:28:18 -0700

    Removed references no used

--- object-oriented/build.bat
+++ object-oriented/build.bat
@@ -1,6 +1,5 @@
-
 echo Compiling Application
-"C:\Program Files (x86)\Microsoft SDKs\F#\3.0\Framework\v4.0\Fsc.exe" -r FSharp.Data.TypeProviders.dll -r System.Data.Linq.dll main.fs
+"C:\Program Files (x86)\Microsoft SDKs\F#\3.0\Framework\v4.0\Fsc.exe" main.fs
 
 echo Running Application
 main.exe
